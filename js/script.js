b4e8c056d60a912d2c899aacb50f1ecbb19f7ec7
Forklein <giuseppepisani2@gmail.com>
2021-08-26 17:11:49 +0200

    Add filter function

--- js/script.js
+++ js/script.js
@@ -7,6 +7,18 @@ const app = new Vue({
         test: 'Hello Vue',
         user: user,
         contacts: contacts,
+        search: '',
+        chat: ["Ciao", "Come stai?"],
     },
-    methods: {},
+    methods: {
+        showItem(contact) {
+            if (!this.search) {
+                return true;
+            }
+            if (contact.toLowerCase().includes(this.search.trim().toLowerCase())) {
+                return true;
+            }
+        }
+
+    },
 });
